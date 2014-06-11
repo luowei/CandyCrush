04a92ce9f144162796511544b4a47eb2bf34db05
luowei <luowei010101@gmail.com>
2014-06-11 10:07:38 +0800

    Initial Commit

---
+++ CandyCrush/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CandyCrush
+//
+//  Created by luowei on 14-6-11.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
